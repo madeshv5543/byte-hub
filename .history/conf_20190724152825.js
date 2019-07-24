d0eeb36edc7035640e3cc24165f27c78837118b5
madeshv5543 <madeshv5543@gmail.com>
2019-07-24 15:48:56 +0530

    inita

---
+++ .history/conf_20190724152825.js
@@ -0,0 +1,46 @@
+/*jslint node: true */
+"use strict";
+
+exports.clientName = 'byteball';
+exports.minClientVersion = '2.7.2';
+
+// https://console.developers.google.com
+exports.pushApiProjectNumber = 0;
+exports.pushApiKey = '';
+
+// iOS Push Notifications APNS config
+exports.APNsAuthKey = ''; // *.p8 filepath or buffer with key itself
+exports.keyId = '';
+exports.teamId = '';
+
+exports.port = 6611;
+//exports.myUrl = 'wss://mydomain.com/bb';
+exports.bServeAsHub = true;
+exports.bSaveJointJson = true;
+exports.bLight = false;
+
+// this is used by wallet vendor only, to redirect bug reports to developers' email
+exports.bug_sink_email = 'admin@example.org';
+exports.bugs_from_email = 'bugs@example.org';
+
+exports.HEARTBEAT_TIMEOUT = 300*1000;
+
+exports.storage = 'sqlite';
+
+
+exports.initial_witnesses = !process.env.testnet ? [
+	
+]
+: [
+
+];
+
+exports.initial_peers = [
+	process.env.testnet ? 'wss://obyte.org/bb-test' : 'wss://obyte.org/bb'
+];
+
+exports.trustedRegistries = {
+	'AM6GTUKENBYA54FYDAKX2VLENFZIMXWG': 'market'
+};
+
+console.log('finished hub conf');
